cc2353bec37a9f60808973c5eeba9a4ff2290a53
bmeinert8 <brian.meinert1@gmail.com>
2025-03-04 10:07:31 -0500

    update server.js to fix local host error issues

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express(); // create express app
+
+app.use(express.static(path.join(__dirname, '.')));
+app.use('/CSS' , express.static(path.join(__dirname + '/CSS')));
+app.use('/images' , express.static(path.join(__dirname + '/images')));
+
 app.get('/', (req, res) =>
   res.sendFile(__dirname + '/index.html'));
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
