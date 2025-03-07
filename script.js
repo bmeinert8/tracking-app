cfb6480007402a882648e02fe7d127bb4ed54a8f
bmeinert8 <brian.meinert1@gmail.com>
2025-03-07 08:40:01 -0500

    refactor script.js and server.js to improve API call handling and add commit grid functionality

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
-
 fetch('https://api.github.com/users/bmeinert8/events', {
   headers: {
-    'Authorization': 'token ' + process.env.GITHUB_TOKEN
+    'Authorization': 'token 
   }
 })
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+  console.log(data); // Log the data to inspect it
+})
 .catch(error => console.error('Error:', error));
+
+const commitGrid = document.querySelector('.js-commit-grid');
+for (let i = 0; i< 365; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('commit-cell');
+  commitGrid.appendChild(cell);
+}
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+// require('dotenv').config();
 const app = express(); // create express app
 
+// Serve static files
 app.use(express.static(path.join(__dirname, '.')));
 app.use('/CSS' , express.static(path.join(__dirname + '/CSS')));
 app.use('/images' , express.static(path.join(__dirname + '/images')));
 
+// Serve the index.html
 app.get('/', (req, res) =>
   res.sendFile(__dirname + '/index.html'));
+
+/*
+fetch('https://api.github.com/users/bmeinert8/events', {
+  headers: {
+    'Authorization': 'token ' + process.env.GITHUB_TOKEN
+  }
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+*/
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
